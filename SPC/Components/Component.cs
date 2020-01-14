0e8ba5d8a44155f98e193b309c0146be4eff20eb
simon.leitl <simon.leitl@t-online.de>
2020-01-14 10:54:19 +0100

    Setter Methoden für Name, Beschreibung, Symbol hinzugefügt

--- SPC/Components/Component.cs
+++ SPC/Components/Component.cs
@@ -8,5 +8,28 @@ namespace SPC.Components
 {
    public abstract class Component
     {
+        private string name;
+        private string beschreibung;
+        private string symbol;
+
+        public Component()
+        {
+
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public void setBeschreibung(String beschreibung)
+        {
+            this.beschreibung = beschreibung;
+        }
+
+        public void setSymbol(String symbol)
+        {
+            this.symbol = symbol;
+        }
     }
 }
